723766f3b09b02ec17cb726ca81a50642623ecd8
William Lee <wjl12@comcast.net>
2015-06-17 11:12:52 -0400

    Initial Commit

---
+++ mynewapp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  mynewapp
+//
+//  Created by William Lee on 6/17/15.
+//  Copyright © 2015 imagios. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
